458bcd9b6e53f292c9d7e5dc52fa85e19580c45b
Kazuya Takei <myself@attakei.net>
2026-07-17 02:41:53 +0900

    fix: keep `None` when directive doesn't have option value

--- src/atsphinx/stlite/directives.py
+++ src/atsphinx/stlite/directives.py
@@ -7,6 +7,7 @@ import json
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+from docutils.parsers.rst import directives
 from sphinx.util import logging
 from sphinx.util.docutils import SphinxDirective
 
@@ -46,7 +47,7 @@ class StliteDirective(SphinxDirective):  # noqa: D101
     option_spec = {
         "config": parsed_dict,
         "requirements": list_of_str,
-        "height": str,
+        "height": directives.unchanged,
     }
     DEFAULT_OPTIONS = {
         "config": None,
